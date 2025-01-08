f7660f28b9cdd08cd3883fadaa42a619b6328fd2
Filip Tasadan <filipt19@g3112.hyak.local>
2025-01-08 07:00:53 -0800

    Simultaneous Text Decoder Class created and Unit CTC Decoder class seaprated from the Simultaneous Text Decoder class

--- researches/ctc_unity/models/streamspeech_model.py
+++ researches/ctc_unity/models/streamspeech_model.py
@@ -6,6 +6,7 @@
 import copy
 import logging
 from pathlib import Path
+from ctc_unity.modules.simultaneous_text_decoder import SimultaneousTextDecoder
 from ctc_unity.models.streaming_speech_encoder import StreamingSpeechEncoder
 import torch
 from typing import OrderedDict
@@ -26,7 +27,6 @@ from fairseq.models.speech_to_speech.s2s_transformer import (
     s2ut_architecture_base,
 )
 from fairseq.models.transformer import TransformerModelBase
-from ctc_unity.modules.transformer_decoder import TransformerDecoder
 from ctc_unity.modules.unit_ctc_decoder import UnitCTCDecoder
 
 from fairseq import checkpoint_utils, utils
@@ -35,15 +35,6 @@ from fairseq.models.transformer import TransformerConfig
 from fairseq.models import FairseqEncoderDecoderModel
 
 logger = logging.getLogger(__name__)
-
-def multitask_text_transformer_decoder_arch(
-    args, decoder_layers, decoder_embed_dim=256, decoder_attention_heads=4
-):
-    args.decoder_layers = decoder_layers
-    args.decoder_embed_dim = decoder_embed_dim
-    args.decoder_attention_heads = decoder_attention_heads
-    base_multitask_text_transformer_decoder_arch(args)
-
 
 @register_model("streamspeech")
 class StreamSpeechModel(FairseqEncoderDecoderModel):
@@ -262,11 +253,11 @@ class StreamSpeechModel(FairseqEncoderDecoderModel):
     @classmethod
     def build_model(cls, args, task):
         streaming_speech_encoder = cls._build_streaming_speech_encoder(args)
-        decoder = cls._build_unit_ctc_decoder(
+        unit_ctc_decoder = cls._build_unit_ctc_decoder(
             args,
             task.target_dictionary
         )
-        base_model = cls(streaming_speech_encoder, decoder)
+        base_model = cls(streaming_speech_encoder, unit_ctc_decoder)
 
         base_model.t2u_augmented_cross_attn = getattr(
             args, "synthesizer_augmented_cross_attention", False
@@ -286,7 +277,8 @@ class StreamSpeechModel(FairseqEncoderDecoderModel):
                 if task_obj.args.input_from == "encoder"
                 else args.decoder_embed_dim
             )
-            task_decoder = cls.build_multitask_decoder(
+
+            task_decoder = cls._build_multitask_decoder(
                 task_obj.args,
                 task_obj.target_dictionary,
                 in_dim,
@@ -296,11 +288,12 @@ class StreamSpeechModel(FairseqEncoderDecoderModel):
             )
 
             setattr(base_model, f"{task_name}_decoder", task_decoder)
-            decoder_model_cls = (
-                FairseqEncoderModel
-                if task_obj.args.decoder_type == "ctc"
-                else FairseqLanguageModel
-            )
+
+            if task_obj.args.decoder_type == "ctc":
+                decoder_model_cls = FairseqEncoderModel
+            else:
+                decoder_model_cls = FairseqLanguageModel
+            
             base_model.multitask_decoders[task_name] = decoder_model_cls(
                 getattr(base_model, f"{task_name}_decoder")
             )
@@ -309,6 +302,7 @@ class StreamSpeechModel(FairseqEncoderDecoderModel):
 
         # set up encoder on top of the auxiliary MT decoder
         if getattr(args, "synthesizer_encoder_layers", 0) > 0:
+            #TODO: clean this method
             base_model.synthesizer_encoder = cls.build_text_encoder(args)
         else:
             base_model.synthesizer_encoder = None
@@ -334,9 +328,137 @@ class StreamSpeechModel(FairseqEncoderDecoderModel):
             )
 
         return base_model
+    
+    def forward(
+        self,
+        src_tokens,
+        src_lengths,
+        prev_output_tokens,
+        prev_output_tokens_mt,
+        streaming_config=None,
+        tgt_speaker=None,
+        return_all_hiddens=False,
+    ):
+        mt_decoder = getattr(self, f"{self.mt_task_name}_decoder")
+        encoder_out = self.encoder(
+            src_tokens,
+            src_lengths=src_lengths,
+            tgt_speaker=tgt_speaker,
+            return_all_hiddens=return_all_hiddens,
+        )
+
+        if streaming_config is not None:
+            asr_decoder = getattr(self, "source_unigram_decoder")
+            asr_ctc_out = asr_decoder(encoder_out["encoder_out"][0].detach())
+            asr_probs = self.get_normalized_probs(
+                [asr_ctc_out["encoder_out"].transpose(0, 1)], log_probs=False
+            )
+            asr_repeat = (
+                torch.cat(
+                    (
+                        torch.zeros(
+                            (asr_probs.size(0), 1, asr_probs.size(-1) - 1),
+                            device=asr_probs.device,
+                        ),
+                        asr_probs[:, :-1, 1:],
+                    ),
+                    dim=1,
+                )
+                * asr_probs[:, :, 1:]
+            )
+            asr_repeat = asr_repeat.sum(dim=-1, keepdim=False)
+            asr_blank = asr_probs[:, :, 0]
+            asr_not_blank = 1 - (asr_repeat + asr_blank).detach()
+
+            st_decoder = getattr(self, "ctc_target_unigram_decoder")
+            st_ctc_out = st_decoder(encoder_out["encoder_out"][0].detach())
+            st_probs = self.get_normalized_probs(
+                [st_ctc_out["encoder_out"].transpose(0, 1)], log_probs=False
+            )
+            st_repeat = (
+                torch.cat(
+                    (
+                        torch.zeros(
+                            (st_probs.size(0), 1, st_probs.size(-1) - 1),
+                            device=st_probs.device,
+                        ),
+                        st_probs[:, :-1, 1:],
+                    ),
+                    dim=1,
+                )
+                * st_probs[:, :, 1:]
+            )
+            st_repeat = st_repeat.sum(dim=-1, keepdim=False)
+            st_blank = st_probs[:, :, 0]
+            st_not_blank = 1 - (st_repeat + st_blank).detach()
+
+            streaming_mask = self._build_streaming_mask(
+                asr_not_blank,
+                st_not_blank,
+                prev_output_tokens_mt,
+                streaming_config["k1"],
+                streaming_config["n1"],
+                streaming_config["n1"],
+            )
+            streaming_config["streaming_mask"] = streaming_mask
+
+        # 1. MT decoder
+        mt_decoder_out = mt_decoder(
+            prev_output_tokens_mt,
+            encoder_out=encoder_out,
+            streaming_config=streaming_config,
+        )
+        x = mt_decoder_out[1]["inner_states"][-1]
+        if mt_decoder.layer_norm is not None:
+            x = mt_decoder.layer_norm(x)
+
+        mt_decoder_padding_mask = None
+        if prev_output_tokens_mt.eq(mt_decoder.padding_idx).any():
+            mt_decoder_padding_mask = prev_output_tokens_mt.eq(mt_decoder.padding_idx)
+
+        # 2. T2U encoder
+        if self.synthesizer_encoder is not None:
+            t2u_encoder_out = self.synthesizer_encoder(
+                x,
+                mt_decoder_padding_mask,
+                return_all_hiddens=return_all_hiddens,
+            )
+        else:
+            t2u_encoder_out = {
+                "encoder_out": [x],  # T x B x C
+                "encoder_padding_mask": [mt_decoder_padding_mask],  # B x T
+            }
+
+        # 3. T2U decoder
+        if self.t2u_augmented_cross_attn:
+            decoder_out = self.decoder(
+                prev_output_tokens,
+                encoder_out=encoder_out,
+                encoder_out_aug=t2u_encoder_out,
+            )
+        else:
+            decoder_out = self.decoder(
+                prev_output_tokens,
+                encoder_out=t2u_encoder_out,
+                streaming_config=(
+                    {
+                        "src_wait": int(streaming_config["k2"]),
+                        "src_step": int(streaming_config["n2"]),
+                    }
+                    if streaming_config is not None
+                    else None
+                ),
+            )
+        if return_all_hiddens:
+            decoder_out[-1]["encoder_states"] = encoder_out["encoder_states"]
+            decoder_out[-1]["encoder_padding_mask"] = encoder_out[
+                "encoder_padding_mask"
+            ]
+        decoder_out[-1]["mt_decoder_out"] = mt_decoder_out
+        return decoder_out
 
     @classmethod
-    def build_multitask_decoder(
+    def _build_multitask_decoder(
         cls,
         args,
         tgt_dict,
@@ -348,14 +470,14 @@ class StreamSpeechModel(FairseqEncoderDecoderModel):
         decoder_args = args.decoder_args
         decoder_args.encoder_embed_dim = in_dim
         if args.decoder_type == "transformer":
-            multitask_text_transformer_decoder_arch(
+            cls._build_simultaneous_text_decoder_arguments(
                     decoder_args,
                     decoder_layers,
                     decoder_embed_dim,
                     decoder_attention_heads,
-                )  # 4L
+                ) 
                 
-            task_decoder = TransformerDecoder(
+            task_decoder = SimultaneousTextDecoder(
                 TransformerConfig.from_namespace(decoder_args),
                 tgt_dict,
                 embed_tokens=TransformerModelBase.build_embedding(
@@ -421,136 +543,15 @@ class StreamSpeechModel(FairseqEncoderDecoderModel):
         _args.encoder_attention_heads = args.decoder_attention_heads
         _args.encoder_normalize_before = True
         return UniTransformerEncoderNoEmb(_args)
+    
+    @classmethod
+    def _build_simultaneous_text_decoder_arguments(cls, args, decoder_layers, decoder_embed_dim=256, decoder_attention_heads=4):
+        args.decoder_layers = decoder_layers
+        args.decoder_embed_dim = decoder_embed_dim
+        args.decoder_attention_heads = decoder_attention_heads
+        base_multitask_text_transformer_decoder_arch(args)
 
-    def forward(
-        self,
-        src_tokens,
-        src_lengths,
-        prev_output_tokens,
-        prev_output_tokens_mt,
-        streaming_config=None,
-        tgt_speaker=None,
-        return_all_hiddens=False,
-    ):
-        mt_decoder = getattr(self, f"{self.mt_task_name}_decoder")
-        encoder_out = self.encoder(
-            src_tokens,
-            src_lengths=src_lengths,
-            tgt_speaker=tgt_speaker,
-            return_all_hiddens=return_all_hiddens,
-        )
-
-        if streaming_config is not None:
-            asr_decoder = getattr(self, "source_unigram_decoder")
-            asr_ctc_out = asr_decoder(encoder_out["encoder_out"][0].detach())
-            asr_probs = self.get_normalized_probs(
-                [asr_ctc_out["encoder_out"].transpose(0, 1)], log_probs=False
-            )
-            asr_repeat = (
-                torch.cat(
-                    (
-                        torch.zeros(
-                            (asr_probs.size(0), 1, asr_probs.size(-1) - 1),
-                            device=asr_probs.device,
-                        ),
-                        asr_probs[:, :-1, 1:],
-                    ),
-                    dim=1,
-                )
-                * asr_probs[:, :, 1:]
-            )
-            asr_repeat = asr_repeat.sum(dim=-1, keepdim=False)
-            asr_blank = asr_probs[:, :, 0]
-            asr_not_blank = 1 - (asr_repeat + asr_blank).detach()
-
-            st_decoder = getattr(self, "ctc_target_unigram_decoder")
-            st_ctc_out = st_decoder(encoder_out["encoder_out"][0].detach())
-            st_probs = self.get_normalized_probs(
-                [st_ctc_out["encoder_out"].transpose(0, 1)], log_probs=False
-            )
-            st_repeat = (
-                torch.cat(
-                    (
-                        torch.zeros(
-                            (st_probs.size(0), 1, st_probs.size(-1) - 1),
-                            device=st_probs.device,
-                        ),
-                        st_probs[:, :-1, 1:],
-                    ),
-                    dim=1,
-                )
-                * st_probs[:, :, 1:]
-            )
-            st_repeat = st_repeat.sum(dim=-1, keepdim=False)
-            st_blank = st_probs[:, :, 0]
-            st_not_blank = 1 - (st_repeat + st_blank).detach()
-
-            streaming_mask = self.build_streaming_mask(
-                asr_not_blank,
-                st_not_blank,
-                prev_output_tokens_mt,
-                streaming_config["k1"],
-                streaming_config["n1"],
-                streaming_config["n1"],
-            )
-            streaming_config["streaming_mask"] = streaming_mask
-
-        # 1. MT decoder
-        mt_decoder_out = mt_decoder(
-            prev_output_tokens_mt,
-            encoder_out=encoder_out,
-            streaming_config=streaming_config,
-        )
-        x = mt_decoder_out[1]["inner_states"][-1]
-        if mt_decoder.layer_norm is not None:
-            x = mt_decoder.layer_norm(x)
-
-        mt_decoder_padding_mask = None
-        if prev_output_tokens_mt.eq(mt_decoder.padding_idx).any():
-            mt_decoder_padding_mask = prev_output_tokens_mt.eq(mt_decoder.padding_idx)
-
-        # 2. T2U encoder
-        if self.synthesizer_encoder is not None:
-            t2u_encoder_out = self.synthesizer_encoder(
-                x,
-                mt_decoder_padding_mask,
-                return_all_hiddens=return_all_hiddens,
-            )
-        else:
-            t2u_encoder_out = {
-                "encoder_out": [x],  # T x B x C
-                "encoder_padding_mask": [mt_decoder_padding_mask],  # B x T
-            }
-
-        # 3. T2U decoder
-        if self.t2u_augmented_cross_attn:
-            decoder_out = self.decoder(
-                prev_output_tokens,
-                encoder_out=encoder_out,
-                encoder_out_aug=t2u_encoder_out,
-            )
-        else:
-            decoder_out = self.decoder(
-                prev_output_tokens,
-                encoder_out=t2u_encoder_out,
-                streaming_config=(
-                    {
-                        "src_wait": int(streaming_config["k2"]),
-                        "src_step": int(streaming_config["n2"]),
-                    }
-                    if streaming_config is not None
-                    else None
-                ),
-            )
-        if return_all_hiddens:
-            decoder_out[-1]["encoder_states"] = encoder_out["encoder_states"]
-            decoder_out[-1]["encoder_padding_mask"] = encoder_out[
-                "encoder_padding_mask"
-            ]
-        decoder_out[-1]["mt_decoder_out"] = mt_decoder_out
-        return decoder_out
-
-    def build_streaming_mask(self, asr, st, y, src_wait, src_step, tgt_step):
+    def _build_streaming_mask(self, asr, st, y, src_wait, src_step, tgt_step):
         tgt_len = y.size(1)
         bsz, src_len = st.size()
         idx = torch.arange(0, tgt_len, device=st.device).unsqueeze(0).unsqueeze(2)
