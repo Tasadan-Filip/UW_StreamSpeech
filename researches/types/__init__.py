9d99e16965c947ca48dca3e2a44c13efc80ffb6a
Pawel Bilko <pablo1785@wp.pl>
2025-01-22 13:19:02 -0800

    Regenerate type stubs

--- researches/types/__init__.py
+++ researches/types/__init__.py
@@ -1,22 +1,24 @@
 from typing import Callable, Literal, TypeVar
 import typing
-from fairseq.tasks import register_task, FairseqTask, FairseqDataclass
+from fairseq.dataclass import FairseqDataclass
+from fairseq.tasks import register_task
 from fairseq.models import register_model
+from fairseq.tasks.fairseq_task import FairseqTask
 from fairseq.utils import get_activation_fn
 from torch import Tensor
 
 TaskType = TypeVar("TaskType", bound=FairseqTask)
 TaskRetType = Callable[[TaskType], type[TaskType]]
 
-def register_task_uw(name, dataclass: FairseqDataclass | None = None) -> TaskRetType:
-    return typing.cast(TaskRetType, register_task(name, dataclass))
+def register_task_uw(name, dataclass: FairseqDataclass | None = None) -> TaskRetType[TaskType]:
+    return typing.cast(TaskRetType[TaskType], register_task(name, dataclass))
 
 
 ModelType = TypeVar("ModelType", bound=FairseqTask)
 ModelRetType = Callable[[ModelType], type[ModelType]]
 
-def register_model_uw(name, dataclass: FairseqDataclass | None = None) -> ModelRetType:
-    return typing.cast(ModelRetType, register_model(name, dataclass))
+def register_model_uw(name, dataclass: FairseqDataclass | None = None) -> ModelRetType[ModelType]:
+    return typing.cast(ModelRetType[ModelType], register_model(name, dataclass))
 
 
 def get_activation_fn_uw(name:  Literal["relu", "relu_squared", "gelu", "gelu_fast", "gelu_accurate", "tanh", "linear", "swish"])-> Callable[[Tensor], Tensor]:
